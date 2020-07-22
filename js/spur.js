65fb7beef44f4a49f6ae8805ca4626cd6b34c428
anmolk18 <anmolpreet.kaur@yale.edu>
2020-07-22 13:32:34 -0400

    basic home page layout done

---
+++ js/spur.js
@@ -0,0 +1,35 @@
+/*!
+ * spur-template - An admin template based on Bootstrap 4
+ * Version v1.1.0
+ * Copyright 2016 - 2019 Alexander Rechsteiner
+ * https://hackerthemes.com
+ */
+
+const mobileBreakpoint = window.matchMedia("(max-width: 991px )");
+
+$(document).ready(function(){
+    $(".dash-nav-dropdown-toggle").click(function(){
+        $(this).closest(".dash-nav-dropdown")
+            .toggleClass("show")
+            .find(".dash-nav-dropdown")
+            .removeClass("show");
+
+        $(this).parent()
+            .siblings()
+            .removeClass("show");
+    });
+
+    $(".menu-toggle").click(function(){
+        if (mobileBreakpoint.matches) {
+            $(".dash-nav").toggleClass("mobile-show");
+        } else {
+            $(".dash").toggleClass("dash-compact");
+        }
+    });
+
+    
+
+    // Dev utilities
+    // $("header.dash-toolbar .menu-toggle").click();
+    // $(".searchbox-toggle").click();
+});
